cc1acfd7c87db6339dc98e6277af26f88010149a
SKMBOSS <zzangjungsu@gmail.com>
2015-10-22 00:46:06 +0900

    6주차 실습

---
+++ src/kc/ac/kookmin/exception/intro/Test.java
@@ -0,0 +1,59 @@
+package kc.ac.kookmin.exception.intro;
+
+import java.io.*;
+import java.util.Scanner;
+
+class AAA {
+	public void readFile() {
+		String path = AAA.class.getResource("").getPath();
+		String fname = path + "a.text";
+
+		File f;
+
+		try {
+			// 파일 객체 생성
+			f = new File(fname);
+
+//			 //파일쓰기
+//			 FileWriter fileWriter = new FileWriter(f.toString(), true); //이어쓰기
+//			 BufferedWriter writer = new BufferedWriter(fileWriter);
+//			
+//			 Scanner sc = new Scanner(System.in);
+//			 String str;
+//			
+//			 System.out.print("a.text에 추가할 거 써봐라: ");
+//			 str = sc.next();
+//			
+//			 writer.newLine();
+//			 writer.write(str);
+//			 writer.close();
+
+			// 파일읽기
+			FileReader fileReader = new FileReader(f);
+			BufferedReader reader = new BufferedReader(fileReader);
+
+			String line = null;
+			while ((line = reader.readLine()) != null) {
+				System.out.println(line);
+			}
+
+		} catch (Exception ex) {
+			System.out.println(ex.getMessage());
+		} finally {
+			close();
+		}
+
+	}
+
+	void close() {
+		System.out.close();
+	}
+}
+
+public class Test {
+	public static void main(String[] args) {
+		AAA a = new AAA();
+		a.readFile();
+	}
+
+}
